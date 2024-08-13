edab2ce0ff2c1a59f5cfd10954620239be26bdb2
arielgpaz <arielgdapaz@gmail.com>
2024-08-13 10:50:15 -0300

    add user request e response

--- src/main/java/com/agpaz/webfluxcourse/controller/UserController.java
+++ src/main/java/com/agpaz/webfluxcourse/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.agpaz.webfluxcourse.controller;
 
+import com.agpaz.webfluxcourse.model.request.UserRequest;
+import com.agpaz.webfluxcourse.model.response.UserResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
---
+++ src/main/java/com/agpaz/webfluxcourse/model/response/UserResponse.java
@@ -0,0 +1,8 @@
+package com.agpaz.webfluxcourse.model.response;
+
+public record UserResponse(
+        String id,
+        String name,
+        String email
+) {
+}
