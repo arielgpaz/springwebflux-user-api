f7996458bc469d09a53872da210935179c1fd8c7
arielgpaz <arielgdapaz@gmail.com>
2024-08-13 19:49:43 -0300

    add validations to user request

--- src/main/java/com/agpaz/webfluxcourse/controller/exception/ControllerExceptionHandler.java
+++ src/main/java/com/agpaz/webfluxcourse/controller/exception/ControllerExceptionHandler.java
@@ -3,8 +3,10 @@ package com.agpaz.webfluxcourse.controller.exception;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.support.WebExchangeBindException;
 import reactor.core.publisher.Mono;
 
 import static java.time.LocalDateTime.now;
@@ -14,7 +16,8 @@ import static org.springframework.http.HttpStatus.BAD_REQUEST;
 public class ControllerExceptionHandler {
 
     @ExceptionHandler(DuplicateKeyException.class)
-    ResponseEntity<Mono<StandardError>> handleDuplicateKeyException(DuplicateKeyException e, ServerHttpRequest request) {
+    ResponseEntity<Mono<StandardError>> handleDuplicateKeyException(
+            DuplicateKeyException e, ServerHttpRequest request) {
         return ResponseEntity.badRequest()
                 .body(Mono.just(StandardError.builder()
                                 .timestamp(now())
@@ -23,6 +26,23 @@ public class ControllerExceptionHandler {
                                 .message(this.verifyDupKey(e.getMessage()))
                                 .path(request.getPath().toString())
                         .build()));
+    }
+
+    @ExceptionHandler(WebExchangeBindException.class)
+    public ResponseEntity<Mono<ValidationError>> handleWebExchangeBindException(
+            WebExchangeBindException e, ServerHttpRequest request) {
+
+        ValidationError error = new ValidationError(
+                now(), request.getPath().toString(), BAD_REQUEST.value(),
+                "Validation error", "Error on atributes validation"
+        );
+
+        for (FieldError fieldError : e.getBindingResult().getFieldErrors()) {
+            error.addError(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+
+        return ResponseEntity.badRequest()
+                .body(Mono.just(error));
     }
 
     private String verifyDupKey(String message) {
--- src/main/java/com/agpaz/webfluxcourse/model/request/UserRequest.java
+++ src/main/java/com/agpaz/webfluxcourse/model/request/UserRequest.java
@@ -1,8 +1,22 @@
 package com.agpaz.webfluxcourse.model.request;
 
+import com.agpaz.webfluxcourse.validator.TrimString;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record UserRequest(
+
+        @Size(min = 2, max = 50)
+        @NotBlank
+        @TrimString
         String name,
+
+        @Email
         String email,
+
+        @Size(min = 4, max = 16)
+        @TrimString
         String password
 ) {
 }
